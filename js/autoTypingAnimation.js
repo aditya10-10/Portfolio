95b5a3756047aa3561474830718a28c7c8be647c
ADITYA CHAUHAN <100583010+aditya10-10@users.noreply.github.com>
2023-04-12 00:38:33 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        // ! this is for the paragraph element down the pfp
+        "<span>Hi, I am Aditya Chauhan,</span>I am excited to embark on my career and make a positive impact in my industry. With a degree in Bachelor of Technology (BTech), I have developed a strong foundation in Java and am eager to apply my knowledge to a professional setting. My passion for Software Engineering drives me to constantly seek out new opportunities for growth and development. In my free time, I enjoy playing kabaddi and am actively involved in Sports. I am currently seeking entry-level opportunities in Software Engineering and am open to new challenges and opportunities for growth. Feel free to connect with me if you have any potential leads or would like to collaborate."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
